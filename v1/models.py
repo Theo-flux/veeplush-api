648e632ab196052c54a68e7119f708f8c3f8baf9
Theo <ekunnusitheophilus@gmail.com>
2023-09-20 21:07:13 +0100

    database model

--- v1/models.py
+++ v1/models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Integer, String, Sequence, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Sequence, Float, ForeignKey, Enum
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from sqlalchemy.dialects.postgresql import JSONB
@@ -8,9 +9,9 @@ from utils.db import Base
 
 
 class Customer(Base):
-    __tablename__ = "cutsomers"
+    __tablename__ = "customers"
 
-    id = Column(Integer, Sequence('customer_seq'), primary_key=True) 
+    id = Column(Integer, Sequence('customer_seq'), primary_key=True, nullable=False) 
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
@@ -25,21 +26,56 @@ class Customer(Base):
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
     updated_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
 
+    # one-to-many
+    orders = relationship('Order', back_populates='customer')
+
 
 class ProductCategory(Base):
     __tablename__ = "product_categories"
 
-    id = Column(Integer, Sequence('product_cat_seq'), primary_key=True) 
+    id = Column(Integer, Sequence('product_cat_seq'), primary_key=True, nullable=False) 
     name = Column(String, nullable=False)
+
+    # one-to-many relationship from product to product categories
+    products = relationship('Product', back_populates='category')
 
 class Product(Base):
     __tablename__ = "products"
 
-    id = Column(Integer, Sequence('product_seq'), primary_key=True) 
-    product_category_id = Column(Integer, ForeignKey('product_categories.id'))
+    id = Column(Integer, Sequence('product_seq'), primary_key=True, nullable=False) 
+    product_category_id = Column(Integer, ForeignKey('product_categories.id'), nullable=False)
     name = Column(String, nullable=False)
     description = Column(String)
-    price = Column(Float)
-    length = Column(mutable_json_type(dbtype=JSONB, nested=True))
-    style = Column(mutable_json_type(dbtype=JSONB, nested=True))
+    price = Column(Float, nullable=False)
+    length = Column(mutable_json_type(dbtype=JSONB, nested=True), nullable=False)
+    style = Column(mutable_json_type(dbtype=JSONB, nested=True), nullable=False)
     stock_qty = Column(Integer)
+
+    # many-to-one relationship from product to product categories
+    category = relationship('ProductCategory', back_populates = 'products')
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, Sequence('order_seq'), primary_key=True, nullable=False)
+    customer_id = Column(Integer, ForeignKey('customers.id'), nullable=False)
+    order_status = Column(Enum('Pending', 'delivered', 'canceled', 'shipped', name='order_status'))
+    order_date = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
+    total_amount = Column(Float, nullable=False)
+
+    # many-to-one relationship from order to customer
+    customer = relationship('Customer', back_populates='orders')
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, Sequence('order_seq'), primary_key=True)
+    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
+    sub_total = Column(Float, nullable=False)
+    qty = Column(Integer, nullable=False)
+
+    # many-to-one relationship from order to customer
+    customer = relationship('Customer', back_populates='orders')
