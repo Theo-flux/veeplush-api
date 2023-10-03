ab5c45261600b4b57cad855ef4328281759b41d2
Theo <ekunnusitheophilus@gmail.com>
2023-10-03 07:27:58 +0100

    fix:codebase recovery from remote to local.

--- v1/utils/db.py
+++ v1/utils/db.py
@@ -1,7 +1,7 @@
 from os import environ
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
